6e9e38d6fa26cae0698f573b632dac0e4fd8260d
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-06-10 15:44:49 +0200

    Created CommandsController. Added CmdApiRepository to service injection.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,41 @@
+﻿using CmdApi.Data;
+using CmdApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmdApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICmdApiRepository _repository;
+
+        public CommandsController(ICmdApiRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //Get /api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetCommands();
+
+            return Ok(commandItems);
+        }
+
+        //Get /api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Command>> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
