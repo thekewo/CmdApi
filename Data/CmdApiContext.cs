fc40c4cc94432cfb1c41409c706269c3829c58d2
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-06-10 16:46:57 +0200

    Added CmdApiContext and setup connection to database.

---
+++ Data/CmdApiContext.cs
@@ -0,0 +1,19 @@
+﻿using CmdApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmdApi.Data
+{
+    public class CmdApiContext : DbContext
+    {
+        public CmdApiContext(DbContextOptions<CmdApiContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
